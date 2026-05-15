506976bd0b4509c12bef578588631d603d2b8aa2
Hassanjaved17 <hassanaptech9@gmail.com>
2026-05-15 20:55:54 +0500

    Add Navbar and Footer components to App layout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useState } from 'react';
 
 function Navbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    const [isOpen, setIsOpen] = React.useState(false);
+    return (
+        <>
+            <div className="main flex items-center justify-between p-4 bg-gray-800 text-white">
+                <div className="logo text-2xl font-bold">Logo</div>
+                <div className="nav-items">
+                    <Link to="/" className="mx-2 hover:text-gray-400">Home</Link>
+                    <Link to="/about" className="mx-2 hover:text-gray-400">About</Link>
+                    <Link to="/contact" className="mx-2 hover:text-gray-400">Contact</Link>
+                </div>
+
+
+
+
+
+
+            </div>
+
+
+
+        </>
+    )
 }
 
 export default Navbar
