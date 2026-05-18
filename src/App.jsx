7eec4ff9dbff3de4e2c139e8e601f532a5bff630
Hassanjaved17 <hassanaptech9@gmail.com>
2026-05-18 23:51:56 +0500

    feat: integrate ThemeContext for dynamic theming in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { useContext } from 'react'
+import ThemeContext from './context/ThemeContext'
 import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 
 
 function App() {
+  const { theme } = useContext(ThemeContext)
+
   return (
     <div className="app">
       <BrowserRouter>
+       <div className={theme === 'dark' ? 'bg-gray-900 text-white' : 'bg-white text-gray-900'}>
       <Navbar />
       <Routes>
           <Route path="/" element={<Home />} />
       </Routes>
       <Footer />
+      </div>
       </BrowserRouter>
     </div>
   )
