f111a36f578067da35a523d824d37fb190393694
Hassanjaved17 <hassanaptech9@gmail.com>
2026-06-01 20:32:50 +0500

    practice contextApi in react

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,9 +1,18 @@
-// import React from 'react'
+import { createContext, useState } from 'react';
 
-const UserContext = () => {
-  return (
-    <div>UserContext</div>
-  )
+const UserContext = createContext();
+
+function UserProvider ({children}){
+ const [user, setUser] = useState(null);
+
+const getUser = async () => {
+  try{
+const response =  await fetch("https://jsonplaceholder.typicode.com/posts")
+  }
+
+  catch(error){
+    console.error('Error fetching user data:', error);
+  }
 }
 
-export default UserContext
+}
